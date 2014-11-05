2259d00867f8412705f156ac8e956b66afc7f674
Peter Collins <peter@drifty.com>
2014-11-05 14:37:12 -0600

    Initial Go structure

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"erasure"
+
+	"log"
 )
 
 func main() {
-	erasure.Hello()
+	m := 12
+	k := 8
+	size := 8 * 16
+	// erasure.Hello()
+	code := erasure.NewCode(m, k, size)
+
+	source := make([]byte, size)
+	for i := range source {
+		source[i] = 0x62
+	}
+
+	encoded := code.Encode(source)
+
+	log.Printf("Encoded: %x\n", encoded)
+	srcErrList := []int8{0, 2, 3, 4}
+
+	recovered := code.Decode(encoded, srcErrList)
+	log.Printf("Recovered: %x\n", recovered)
 }
--- src/erasure/erasure.go
+++ src/erasure/erasure.go
@@ -9,8 +9,98 @@ import "C"
 
 import (
 	"fmt"
+	"log"
 	"unsafe"
 )
+
+type Code struct {
+	M            int
+	K            int
+	VectorLength int
+	EncodeMatrix []byte
+	galoisTables []byte
+}
+
+func NewCode(m int, k int, size int) *Code {
+	if m <= 0 || k <= 0 || k >= m || size < 0 {
+		log.Fatal("Invalid erasure code params")
+	}
+	if size%k != 0 {
+		log.Fatal("Size to encode is not divisable by k and therefore cannot be enocded in vector chunks")
+	}
+
+	encodeMatrix := make([]byte, m*k)
+	galoisTables := make([]byte, k*(m-k)*32)
+
+	if k > 5 {
+		C.gf_gen_cauchy1_matrix((*C.uchar)(&encodeMatrix[0]), C.int(m), C.int(k))
+	} else {
+		C.gf_gen_rs_matrix((*C.uchar)(&encodeMatrix[0]), C.int(m), C.int(k))
+	}
+
+	return &Code{
+		M:            m,
+		K:            k,
+		VectorLength: size / k,
+		EncodeMatrix: encodeMatrix,
+		galoisTables: galoisTables,
+	}
+}
+
+// Data buffer to encode must be of the k*size given in the constructor
+// The returned encoded buffer is (m-k)*size, since the first k*size of the
+// encoded data is just the original data due to the identity matrix
+func (c *Code) Encode(data []byte) []byte {
+	if len(data) != c.K*c.VectorLength {
+		log.Fatal("Data to encode is not the proper size")
+	}
+	// Since the first k row of the encode matrix is actually the identity matrix
+	// we only need to encode the last m-k vectors of the matrix and append
+	// them to the original data
+	encoded := make([]byte, (c.M-c.K)*(c.VectorLength))
+	C.ec_init_tables(C.int(c.K), C.int(c.M-c.K), (*C.uchar)(&c.EncodeMatrix[c.K*c.K]), (*C.uchar)(&c.galoisTables[0]))
+	C.ec_encode_data(C.int(c.VectorLength), C.int(c.K), C.int(c.M-c.K), (*C.uchar)(&c.galoisTables[0]), (*C.uchar)(&data[0]), (*C.uchar)(&encoded[0]))
+
+	// return append(data, encoded...)
+	return encoded
+}
+
+// Data buffer to decode must be of the k*size given in the constructor
+// The source error list must contain m-k values, corresponding to the vectors with errors
+// The returned decoded data is k*size
+func (c *Code) Decode(encoded []byte, srcErrList []int8) []byte {
+	if len(encoded) != c.K*c.VectorLength {
+		log.Fatal("Data to decode is not the proper size")
+	}
+	if len(srcErrList) != c.M-c.K {
+		log.Fatal("Err list is not the proper size")
+	}
+	decodeMatrix := make([]byte, c.M*c.K)
+	decodeIndex := make([]int32, c.M)
+	srcInErr := make([]int8, c.M)
+	nErrs := len(srcErrList)
+	nSrcErrs := 0
+	for _, err := range srcErrList {
+		srcInErr[err] = 1
+		if err < int8(c.K) {
+			nSrcErrs++
+		}
+	}
+
+	C.gf_gen_decode_matrix((*C.uchar)(&c.EncodeMatrix[0]), (*C.uchar)(&decodeMatrix[0]), (*C.uint)(unsafe.Pointer(&decodeIndex[0])), (*C.uchar)(unsafe.Pointer(&srcErrList[0])), (*C.uchar)(unsafe.Pointer(&srcInErr[0])), C.int(nErrs), C.int(nSrcErrs), C.int(c.K), C.int(c.M))
+
+	C.ec_init_tables(C.int(c.K), C.int(nErrs), (*C.uchar)(&decodeMatrix[0]), (*C.uchar)(&c.galoisTables[0]))
+
+	recovered := []byte{}
+	for i := 0; i <= c.K; i++ {
+		recovered = append(recovered, encoded[(decodeIndex[i]*int32(c.VectorLength)):(decodeIndex[i]+1)*int32(c.VectorLength)]...)
+	}
+
+	data := make([]byte, c.M*c.VectorLength)
+	C.ec_encode_data(C.int(c.VectorLength), C.int(c.K), C.int(c.M), (*C.uchar)(&c.galoisTables[0]), (*C.uchar)(&recovered[0]), (*C.uchar)(&data[0]))
+
+	return data[:c.K*c.VectorLength]
+}
 
 func Hello() {
 	var m int32 = 12
