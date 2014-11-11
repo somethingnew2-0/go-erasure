68551107fac37a06de5ba006aa1895b62133990d
Peter Collins <peter@drifty.com>
2014-11-11 14:56:28 -0600

    Rebase messed up some stuff

--- erasure.go
+++ erasure.go
@@ -37,16 +37,12 @@ type decodeTrieNode struct {
 }
 
 func (c *Code) getDecode(errList []byte) *decodeTrieNode {
-	var node *decodeTrieNode
-	if len(errList) == 0 {
-		node = c.decodeTrie
-	} else {
-		node = c.decodeTrie.getDecode(errList, 0, byte(c.M))
-	}
+	node := c.decodeTrie.getDecode(errList, 0, byte(c.M))
+
 	node.mutex.Lock()
 	defer node.mutex.Unlock()
 	if node.galoisTables == nil || node.decodeIndex == nil {
-		node.galoisTables = make([]byte, c.K*(c.M-c.K)*32)
+		node.galoisTables = make([]byte, c.K*c.M*32)
 		node.decodeIndex = make([]byte, c.K)
 
 		decodeMatrix := make([]byte, c.M*c.K)
